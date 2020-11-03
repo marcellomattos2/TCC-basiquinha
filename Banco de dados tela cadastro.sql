882d35f7ad6764d47ebe19917d438af6c54fc225
Leticia <61984825+Gaby-leticia@users.noreply.github.com>
2020-11-03 10:58:18 -0300

    Add files via upload

---
+++ Banco de dados tela cadastro.sql
@@ -0,0 +1,14 @@
+create database cadastro;
+
+use cadastro;
+create table clientes (
+nome varchar(30),
+sobrenome varchar(30),
+telefone float,
+email varchar(30),
+senha varchar(30)
+);
+
+describe clientes;
+
+
